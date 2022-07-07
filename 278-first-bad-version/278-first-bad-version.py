518dc814cea99d2fa7952b77af0459adf5544c35
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2022-07-07 23:36:08 +0800

    Time: 54 ms (27.96%), Space: 13.8 MB (96.84%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.py
@@ -0,0 +1,18 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+      start = 1
+      end = n 
+        
+      while (start < end):
+        mid = start + (end - start) // 2 
+        if (isBadVersion(mid)):
+          end = mid 
+        else: 
+          start = mid + 1 
+      return start 
+    
+#Time: O(log n) [binary search]
+#Space: O(1)
