aa3e70827ec5ae8bd75645b13968dee07054fb1b
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2022-07-08 22:53:48 +0800

    Time: 47 ms (50.57%), Space: 13.9 MB (66.48%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,18 +1,17 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-      
       pascalTri = [] 
-      for i in range(1, numRows+1): 
-        if (i == 1):
-          pascalTri += [[1],]
-        elif (i == 2):
-          pascalTri += [[1,1],]
-        else:
-          prevArray = pascalTri[i-2]
-          new = [1,]
-          for i in range (len(prevArray)-1):
-            new += [(prevArray[i] + prevArray[i+1]),]
-          new += [1,]
-          pascalTri += [new,] 
+      for i in range(numRows): 
+        # The first and last row elements are always 1.
+        row = [None for x in range(i + 1)]
+        row[0], row[-1] = 1, 1
+        
+        for j in range (1, len(row)-1):
+          row[j] = pascalTri[i - 1][j - 1] + pascalTri[i - 1][j]
+        pascalTri.append(row)
       return pascalTri
-              
+
+    #DP Problem 
+    #Base Case: start and end = 1 
+    #Recurrence relation: curr = pre[i-1] + prev[i] 
+    #Time complexity: O(numRows^2) 
