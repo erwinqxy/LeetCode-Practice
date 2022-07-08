0c977e3cb2a7972f1ba7ecc4d023a11a1a5f2ece
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2022-07-08 23:32:41 +0800

    Time: 402 ms (31.02%), Space: 16.2 MB (80.67%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -3,3 +3,8 @@ class Solution:
       squared = list(map(lambda x:x**2, nums))
       squared.sort()
       return squared
+    
+    #interesting info: 
+    # Time Complexity: O(nlogn) 
+    #list.sort() function in Python is implemented with the Timsort algorithm whose space complexity is O(n)
+    #the Arrays.sort() in Java is a variant of quicksort algorithm whose space complexity is O(logN).
