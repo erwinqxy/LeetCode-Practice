1af6387ba7eb05467cdd7e1264a06c5671732693
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2022-07-20 01:04:13 +0800

    Time: 118 ms (45.94%), Space: 15.1 MB (50.46%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,24 +1,10 @@
 class Solution:
-    # Attempt 1: 
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-#         for i in range(len(nums)-1):
-#             n1 = nums[i] 
-#             n2_needToBe = target - n1 
-#             if (n2_needToBe in nums[i:]):
-#                 for j in range(i+1, len(nums)):
-#                     n2 = nums[j]
-#                     if (n1 + n2 == target):
-#                         return [i, j]
-
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #preprocess the array into a dict O(n)
-        nums_dict = {} 
-        for i in range (len(nums)):
-            nums_dict[nums[i]] = i 
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
+      
         
-        for i in range (len(nums)):
-            comp = target - nums[i] 
-            if comp in nums_dict and nums_dict[comp] != i:
-                return [i, nums_dict[comp]]
-            
