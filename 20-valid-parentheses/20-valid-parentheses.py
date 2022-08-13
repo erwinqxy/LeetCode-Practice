55a879e545ae6f71318fe267001a9c3e6475877d
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2022-08-13 17:55:32 +0800

    Time: 44 ms (64.55%), Space: 13.9 MB (25.50%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,20 +1,18 @@
 class Solution:
-    def isValid(self, s: str) -> bool:
-        pairings = {')':'(', '}':'{', ']':'['}
-        opens = ['(', '{', '[']
-        
-        stack = "" 
-        
-        for i in range (len(s)):
-          if (s[i]) in opens:
-            stack += s[i]
-          else: 
-            if stack == "":
-              return False 
-            toPop = pairings[s[i]]
-            topOfStack = stack[-1]
-            if toPop != topOfStack:
-              return False 
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        stack = []
+        mappings = {")": "(", "}": "{", "]": "["}
+
+        for char in s:
+            if char in mappings:
+                top_element = stack.pop() if stack else ''
+                if mappings[char] != top_element:
+                    return False
             else:
-              stack = stack[:-1]
-        return stack == "" 
+                stack.append(char)
+        return not stack
