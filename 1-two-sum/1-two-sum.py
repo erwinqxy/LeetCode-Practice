4f731ec0461761df4e8d6381da11996d245f819c
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2022-09-17 16:46:12 +0800

    Time: 117 ms (53.41%), Space: 15.2 MB (24.23%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -3,8 +3,11 @@ class Solution:
       # precommute the array and check back 
       mappings = {} 
       for i in range (len(nums)):
+        # get the value required for the second half 
         rem = target - nums[i] 
+        # check if it already exist in the mapping, if exist great! 
         if (rem in mappings): 
           return [i, mappings[rem]]
+        # else continue storing what you have seen 
         if (nums[i] not in mappings):
           mappings[nums[i]] = i
