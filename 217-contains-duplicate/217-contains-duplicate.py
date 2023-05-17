ccd86bc216dc500ac7e0bbab6cbd4d271655569d
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2023-05-16 22:26:34 -0400

    Update 217-contains-duplicate.py
    Improve approach

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,12 +1,9 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        nums.sort(); 
-        for i in range(len(nums)-1): 
-            if nums[i] == nums[i+1]:
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
                 return True
+            hashset.add(n)
         return False
-        
